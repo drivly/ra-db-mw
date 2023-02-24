0880b6dbc6ccfd12802b6e69295bf374442c1012
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-02-24 06:02:58 -0600

    Updated example

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import dynamic from 'next/dynamic'
+import yaml from 'yaml'
+import fs from 'fs'
 
 const App = dynamic(() => import('../components/App'), { ssr: false })
 
 
 const Home = props => {
   return <App {...props} />
+}
+
+export async function getStaticProps() {
+  const graphdl = yaml.parse(fs.readFileSync('graphdl.yaml', 'utf8'))
+  console.log({graphdl})
+  return {
+    props: {
+      graphdl
+    }
+  }
 }
 
 export default Home
