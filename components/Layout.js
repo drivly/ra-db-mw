dd1c15ba7c041f6a9b76e5eb8f4c6c739ece6e61
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-02-25 04:54:53 -0600

    Updating code locations

--- components/Layout.js
+++ components/Layout.js
@@ -276,11 +276,11 @@ export default function Layout(props) {
           </div>
 
           <main className="flex-1">
-            <div className="py-6">
+            <div className="pt-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">{activeResource}</h1>
               </div>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-[-3em]">
                 {props.children}
               </div>
             </div>
